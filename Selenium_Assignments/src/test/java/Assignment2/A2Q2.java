3ceb9c2f749429b6f74be512c2e2396763de5c08
Jay <jayanthirsundar@gmail.com>
2021-02-19 00:02:11 +0000

    Commiting assignments_1&2 after correction

---
+++ Selenium_Assignments/src/test/java/Assignment2/A2Q2.java
@@ -0,0 +1,60 @@
+package Assignment2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+ /*2) Automate the following scenario
+         a) Navigate to http://uitest.automationtester.uk/drag-and-drop-demo.html
+         b) Implement a test to handle the ‘Items to Drag’ to ‘Drop here’ section and verify the results.*/
+
+/* its not working, problem in the webpage
+
+ */
+public class A2Q2 {
+    WebDriver driver;
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://uitest.automationtester.uk/drag-and-drop-demo.html");
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void testHandleActions() throws InterruptedException {
+        List<WebElement> itemsToDrag = driver.findElements(By.xpath("//span[@draggable='true']"));
+
+        System.out.println("Size:"+ itemsToDrag.size());
+        Thread.sleep(5000);
+
+        Actions action = new Actions(driver);
+        WebElement dragplace = driver.findElement(By.xpath("//div[@class='w50 moveleft'] "));
+        dragplace.click();
+        Thread.sleep(1000);
+        for (WebElement dragItem : itemsToDrag) {
+            action.click(dragItem);
+            Thread.sleep(2000);
+            action.dragAndDrop(dragItem,dragplace).build().perform();
+            Thread.sleep(3000);
+            System.out.println(dragItem.getText());
+
+        }
+        Thread.sleep(3000);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
+//div[@class='w50 moveleft']    //span[@draggable='true']   //div[@id='mydropzone']
---
+++ Selenium_Assignments/src/test/java/Assignment2/A2Q6.java
@@ -0,0 +1,59 @@
+package Assignment2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/*6) Automate the following
+        a) Visit http://uitest.automationtester.uk/bootstrap-download-progress-demo.html
+        b) Implement a suitable synchronization technique
+        to check the download progress percentage and verify until 100% is shown.*/
+public class A2Q6 {
+    WebDriver driver;
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://uitest.automationtester.uk/bootstrap-download-progress-demo.html");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void testSync() throws InterruptedException {
+
+        WebElement download = driver.findElement(By.xpath("//button[@id='cricle-btn']"));
+        Thread.sleep(2000);
+        download.click();
+
+        //waiting until 100% is shown
+
+        WebElement percent = driver.findElement(By.xpath("//div[contains(text(),'0%')]"));
+        WebDriverWait wait = new WebDriverWait(driver,40);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'100%')]")));
+        //or
+        wait.until(ExpectedConditions.textToBePresentInElement(percent,"100%"));
+        System.out.println(percent.getText());
+        Thread.sleep(2000);
+        Assert.assertEquals(percent.getText(),"100%");
+
+
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
